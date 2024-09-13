ba529a6f505ed3ec0b57617944e10eb9749a2e61
HarifVelarde <harif.velarde@gmail.com>
2024-09-12 22:04:52 -0600

    trigger

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. CRM"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elevate.settings')
     try:
         from django.core.management import execute_from_command_line
